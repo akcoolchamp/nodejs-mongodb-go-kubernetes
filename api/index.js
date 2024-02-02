aaf37c2d38f433112400a82ff70d80e8f6ab3720
akshay-scalex <43062880+akshay-scalex@users.noreply.github.com>
2024-02-02 23:25:27 +0530

    Restructured the code

---
+++ api/index.js
@@ -0,0 +1,28 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import app from "./src/app.js";
+
+dotenv.config();
+
+const mongoURI = process.env.MONGODB_URI;
+
+const connectToMongoDB = async () => {
+    try {
+        await mongoose.connect(mongoURI);
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error("Failed to connect to MongoDB:", error);
+    }
+};
+
+connectToMongoDB();
+
+app.get("/", (req, res) => {
+    res.send("Server is running");
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
--- api/src/app.js
+++ api/src/app.js
@@ -1,33 +1,11 @@
-import express from "express";
-import mongoose from "mongoose";
 import dotenv from "dotenv";
-
 dotenv.config();
+
+import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 
-const mongoURI = process.env.MONGODB_URI;
+app.use(bodyParser.json());
 
-const connectToMongoDB = async () => {
-    try {
-        await mongoose.connect(mongoURI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log("Connected to MongoDB");
-    } catch (error) {
-        console.error("Failed to connect to MongoDB:", error);
-    }
-};
-
-connectToMongoDB();
-
-app.get("/", (req, res) => {
-    res.send("Server is running");
-});
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+export default app;
